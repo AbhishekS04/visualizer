0955b38ca32d26fe33d9693bff216c2a2df859ac
AbhishekS04 <abhishek23main@gmail.com>
2025-08-29 23:22:18 +0530

    feat: update project metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ import { Suspense } from "react"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.app",
+  title: "visualizer",
+  description: "Created by uss",
+ 
 }
 
 export default function RootLayout({
